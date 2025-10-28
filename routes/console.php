744ec5bef63f32e9f748ca4892ff16d03e526d0d
xojiakbar <x.madaminov@uzinfocom.uz>
2025-10-28 11:59:31 +0500

    sale and debtor

--- routes/console.php
+++ routes/console.php
@@ -9,4 +9,4 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::command('debtors:send-sms')->dailyAt('9:30')->timezone('Asia/Tashkent');
-Schedule::command('telegram:send-daily-summary')->dailyAt('23:30')->timezone('Asia/Tashkent');
+Schedule::command('telegram:send-daily-summary')->dailyAt('12:02')->timezone('Asia/Tashkent');
